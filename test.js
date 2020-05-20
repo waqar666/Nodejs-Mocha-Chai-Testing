b471cc7095ed64f4283ae2f113e14cc08fbb708e
Waqar Qureshi <waqar666@gmail.com>
2020-05-21 00:19:35 +0530

    Add files via upload

---
+++ test.js
@@ -0,0 +1,13 @@
+/// <reference path = "typings/mocha/mocha.d.ts"/>
+'use strict';
+var chai = require('chai');
+expect = chai.expect;
+chai.should();
+function isEven(num){
+    return num % 2 ===0;
+}
+describe('isEven', function(){
+    it('should return true when number is even', function() {
+        isEven(4).should.be.true;
+    })
+});
